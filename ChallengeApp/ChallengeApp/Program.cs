1094176c89c5223dfed98c3afaae3f1c910f417e
Admin <przemekworonczuk@gmail.com>
2024-12-11 18:04:27 +0100

    zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+char sex = 'K';
+var age = 28;
+
+if (name == "Ewa" && age < 30)
+{
+    Console.WriteLine("Ewa mniej niz 30 lat");
+}
+else if (sex == 'K' && age == 30)
+{
+    Console.WriteLine("Kobieta lat 30");
+}
+else if (sex == 'M' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni meszczyzna");
+}
+else
+{
+    Console.WriteLine("Nie pasuje do żadnych danych");
+}
+

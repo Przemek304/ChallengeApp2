6e7dbd8b4959de0ad139d5ea72c264d90150ba3d
Admin <przemekworonczuk@gmail.com>
2024-12-15 04:25:30 +0100

    zadanie domowe dzień 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,69 +1,44 @@
-﻿int number = 4556;
-string numberAsString = number.ToString();
-char[] signs = numberAsString.ToCharArray();
+﻿
+using ChallengeApp;
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
 
-foreach (char sign in signs)
+Employee employee1 = new Employee("Adam1" ,"Kowalksa" , 20);
+Employee employee2 = new Employee("Paweł", "Kowlaski", 23);
+Employee employee3 = new Employee("Zuzia", "Kowalska", 22);
+
+
+
+employee1.AddScore(5);
+employee1.AddScore(3);
+employee1.AddScore(5);
+
+employee2.AddScore(5);
+employee2.AddScore(1);
+employee2.AddScore(5);
+
+employee3.AddScore(5);
+employee3.AddScore(2);
+employee3.AddScore(7);
+
+List <Employee>employees = new List<Employee>()
 {
-    if (sign == '0')
-    {
-        counter0++;
-    }
-    else if (sign == '1')
-    {
-        counter1++;
-    }
-    else if (sign == '2')
-    {
-        counter2++;
-    }
-    else if (sign == '3')
-    {
-        counter3++;
-    }
-    else if (sign == '4')
-    {
-        counter4++;
-    }
-    else if (sign == '5')
-    {
-        counter5++;
-    }
-    else if (sign == '6')
-    {
-        counter6++;
-    }
-    else if (sign == '7')
-    {
-        counter7++;
-    }
-    else if (sign == '8')
-    {
-        counter8++;
-    }
-    else if(sign == '9')
-    {
-        counter9++;
-    }
+    employee1, employee2, employee3,
+};
 
+int maxResult = -1;
+
+Employee employeeWithMaxResult = null;
+
+foreach (Employee employee in employees)
+{
+    if(employee.Result > maxResult)
+    {
+        maxResult = employee.Result;
+        employeeWithMaxResult = employee;
+    }
 }
-Console.WriteLine("0 ==>" + counter0);
-Console.WriteLine("1 ==>" + counter1);
-Console.WriteLine("2 ==>" + counter2);   
-Console.WriteLine("3 ==>" + counter3);
-Console.WriteLine("4 ==>" + counter4);
-Console.WriteLine("5 ==>" + counter5);
-Console.WriteLine("6 ==>" + counter6);
-Console.WriteLine("7 ==>" + counter7);
-Console.WriteLine("8 ==>" + counter8);
-Console.WriteLine("9 ==>" + counter9);
+
+Console.WriteLine("dane pracownika:" + employeeWithMaxResult.Name + employeeWithMaxResult.Surename + employeeWithMaxResult.Age );
+Console.WriteLine("Największy wynik:" + employeeWithMaxResult.Result);
+
+
